a0f3b50f276e24650ee245dabafb487226d9019e
Nishi Kaza <t-nikaz@microsoft.com>
2019-05-28 10:59:11 -0700

    basic code edit functionality and font size change working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,117 +1,99 @@
 import React from 'react';
-import { DefaultButton, Stack, Text, Link, FontWeights, PrimaryButton } from 'office-ui-fabric-react';
+import { Dropdown, IDropdownOption } from 'office-ui-fabric-react';
 import Editor from 'react-simple-code-editor';
-const boldStyle = { root: { fontWeight: FontWeights.semibold } };
-//const prismLang = require('prismjs/components/prism-typescript');
-const prismHighlight = require('prismjs').highlight
-const code =
-`import React from 'react';
-import { css, classNamesFunction } from 'office-ui-fabric-react/lib/Utilities';
-import { getStyles, IButtonBasicExampleStyleProps, IButtonBasicExampleStyles } from './Button.Basic.Example.styles';
-import { DefaultButton, PrimaryButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
-import { Label } from 'office-ui-fabric-react/lib/Label';
+import * as PrismJS from 'prismjs';
+// const boldStyle = { root: { fontWeight: FontWeights.semibold } };
+// const prismLang = require('prismjs/components/prism-typescript');
+// const prismHighlight = require('prismjs');
+const code = `var yo = 'sdf';`
+const options: IDropdownOption[] = [
+  { key: '12', text: '12'},
+  { key: '14', text: '14'},
+  { key: '16', text: '16'},
+  { key: '18', text: '18'},
+  { key: '20', text: '20'},
+  { key: '22', text: '22'},
+  { key: '24', text: '24'},
+]
+const fontSize = 12;
+// `import React from 'react';
+// import { css, classNamesFunction } from 'office-ui-fabric-react/lib/Utilities';
+// import { getStyles, IButtonBasicExampleStyleProps, IButtonBasicExampleStyles } from './Button.Basic.Example.styles';
+// import { DefaultButton, PrimaryButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
+// import { Label } from 'office-ui-fabric-react/lib/Label';
 
-const getClassNames = classNamesFunction<IButtonBasicExampleStyleProps, IButtonBasicExampleStyles>();
+// const getClassNames = classNamesFunction<IButtonBasicExampleStyleProps, IButtonBasicExampleStyles>();
 
-export class ButtonDefaultExample extends React.Component<IButtonProps, {}> {
-  public render(): JSX.Element {
-    const { disabled, checked } = this.props;
-    const classNames = getClassNames(getStyles, {});
+// export class ButtonDefaultExample extends React.Component<IButtonProps, {}> {
+//   public render(): JSX.Element {
+//     const { disabled, checked } = this.props;
+//     const classNames = getClassNames(getStyles, {});
 
-    return (
-      <div className={css(classNames.twoup)}>
-        <div>
-          <Label>Standard</Label>
-          <DefaultButton
-            data-automation-id="test"
-            allowDisabledFocus={true}
-            disabled={disabled}
-            checked={checked}
-            text="Button"
-            onClick={this._alertClicked}
-          />
-        </div>
-        <div>
-          <Label>Primary</Label>
-          <PrimaryButton
-            data-automation-id="test"
-            disabled={disabled}
-            checked={checked}
-            text="Button"
-            onClick={this._alertClicked}
-            allowDisabledFocus={true}
-          />
-        </div>
-      </div>
-    );
-  }
+//     return (
+//       <div className={css(classNames.twoup)}>
+//         <div>
+//           <Label>Standard</Label>
+//           <DefaultButton
+//             data-automation-id="test"
+//             allowDisabledFocus={true}
+//             disabled={disabled}
+//             checked={checked}
+//             text="Button"
+//             onClick={this._alertClicked}
+//           />
+//         </div>
+//         <div>
+//           <Label>Primary</Label>
+//           <PrimaryButton
+//             data-automation-id="test"
+//             disabled={disabled}
+//             checked={checked}
+//             text="Button"
+//             onClick={this._alertClicked}
+//             allowDisabledFocus={true}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
 
-  private _alertClicked(): void {
-    alert('Clicked');
-  }
-}`;
+//   private _alertClicked(): void {
+//     alert('Clicked');
+//   }
+// }`;
 
 
 export class App extends React.Component{
-  state = { code};
+  state = { code, options, fontSize };
+
+  private changeFontSize = (event: React.FormEvent, option:IDropdownOption | undefined, index: number | undefined): void => {
+    if (typeof(index) != undefined) {
+      this.setState({fontSize: parseInt(options[index].key)})
+    }
+  }
+
   render() {
     return(
+      <div>
       <Editor
-      value = { code }
+      value = { this.state.code }
       onValueChange = {code => this.setState({code})}
-      highlight = {prismHighlight}
+      highlight = {code=>PrismJS.highlight(code, PrismJS.languages.js, 'js')}
+      // highlight = {code=>code}
+      style = {{
+        fontFamily: "Consolas",
+        fontSize: this.state.fontSize,
+      }}
       ></Editor>
+      <Dropdown
+        options = {this.state.options}
+        defaultSelectedKey = '12'
+        onChange = {this.changeFontSize}
+        styles = {{dropdown: {width: 100}}}
+      >
+
+      </Dropdown>
+      </div>
     )
   }
 }
-
-// export const App: React.FunctionComponent = () => {
-//   let state = { code };
-
-//   return (
-//     <Editor
-//       value = { code }
-//       onValueChange = {code => this.setState({code})}
-//       highlight = prismHighlight
-//       ></Editor>
-
-//     // <Stack
-//     //   horizontalAlign="center"
-//     //   verticalAlign="center"
-//     //   verticalFill
-//     //   styles={{
-//     //     root: {
-//     //       width: '960px',
-//     //       margin: '0 auto',
-//     //       textAlign: 'center',
-//     //       color: '#605e5c'
-//     //     }
-//     //   }}
-//     //   gap={15}
-//     // >
-//     //   <img src={logo} alt="logo" />
-//     //   <Text variant="xxLarge" styles={boldStyle}>
-//     //     Welcome to Your UI Fabric App
-//     //   </Text>
-//     //   <Text variant="large">For a guide on how to customize this project, check out the UI Fabric documentation.</Text>
-//     //   <Text variant="large" styles={boldStyle}>
-//     //     Essential Links
-//     //   </Text>
-//     //   <Stack horizontal gap={15} horizontalAlign="center">
-//     //     <Link href="https://developer.microsoft.com/en-us/fabric">Docs</Link>
-//     //     <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">Stack Overflow</Link>
-//     //     <Link href="https://github.com/officeDev/office-ui-fabric-react/">Github</Link>
-//     //     <Link href="https://twitter.com/officeuifabric">Twitter</Link>
-//     //   </Stack>
-//     //   <Text variant="large" styles={boldStyle}>
-//     //     Design System
-//     //   </Text>
-//     //   <Stack horizontal gap={15} horizontalAlign="center">
-//     //     <Link href="https://developer.microsoft.com/en-us/fabric#/styles/icons">Icons</Link>
-//     //     <Link href="https://developer.microsoft.com/en-us/fabric#/styles/typography">Typography</Link>
-//     //     <Link href="https://developer.microsoft.com/en-us/fabric#/styles/themegenerator">Theme</Link>
-//     //   </Stack>
-//     // </Stack>
-
-//   );
-// };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 const { webpackConfig } = require('just-scripts');
-module.exports = webpackConfig;
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const prismStuff = {
+  plugins:[
+    new webpack.ProvidePlugin({
+      highlight: 'prismjs/components/prism-core',
+      languages: 'prismjs/components/prism-core'
+    })
+  ]
+}
+
+module.exports =merge(prismStuff, webpackConfig);
 
 
 
